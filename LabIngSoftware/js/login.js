62c3f94aaf58a67212134d848d1396bb31e0a5e0
JavierGarciaGarc <124699145+JavierGarciaGarc@users.noreply.github.com>
2023-03-07 19:02:36 +0100

    Avances 07/03/2023
    Implementación de la visualización de los videos mediante iframe. Pendiente personalización de la página de inicio de sesión y de la página de visualización de los vídeos.

--- LabIngSoftware/js/login.js
+++ LabIngSoftware/js/login.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 function iniciar() {
    var email = document.getElementById("floatingInput").value
    var pwd = document.getElementById("floatingPassword").value
-
+console.log(pwd)
    var myHeaders = new Headers();
    myHeaders.append("Content-Type", "application/json");
    
@@ -21,7 +21,7 @@ function iniciar() {
      redirect: 'follow'
    };
    
-   fetch("https://restlabingsoft-production.up.railway.app/api/auth/login", requestOptions)
+   fetch("https://restlabingsoft-production-0999.up.railway.app/api/auth/login", requestOptions)
      .then(response => response.json())
      .then(result => {
         console.log(result.token)
